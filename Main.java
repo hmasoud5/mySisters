9460b3530058556a2c2b3f8334248301b31701c5
Heba Masoud <heba.masoud@patelhs.org>
2020-10-08 14:14:39 +0000

    Added boolean

--- Main.java
+++ Main.java
@@ -1,5 +1,8 @@
 class Main {
-  public static void main(String[] args) {
-    System.out.println("Hello world!");
+  public static void main(String[] args)
+   {
+    boolean sisters = true;
+    
+  
   }
 }
